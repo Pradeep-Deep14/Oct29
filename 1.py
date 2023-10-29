cafcc7288fdd1281ee472fec85987959f4d7f05c
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-10-29 10:34:32 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,9 @@
+c= 'hello'
+print(c.center(10,'1'))
+
+
+#11hello111
+#the output of this code is `'11hello111'`,
+#where the original string `'hello'` is 
+# centered within a width of 10 characters, 
+# and the extra space is filled with the character `'1'`
---
+++ 3.py
@@ -0,0 +1,11 @@
+a = 256
+b = 256
+a is b
+
+#True
+
+a = 257
+b = 257
+a is b
+
+#False
